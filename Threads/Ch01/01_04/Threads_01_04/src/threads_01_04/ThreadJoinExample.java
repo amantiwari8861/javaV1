f5eeea58137e3d1a1fc76e4d19c461cf6143e8c9
Aman Tiwari <36691571+amantiwari8861@users.noreply.github.com>
2022-01-28 13:15:08 +0530

    threads added

---
+++ Threads/Ch01/01_04/Threads_01_04/src/threads_01_04/ThreadJoinExample.java
@@ -0,0 +1,31 @@
+/* This program demonstrates the use of some advanced thread 
+ * methods, specifically sleep and join.  This is the main 
+ * method which creates three threads. Then it tries to join 
+ * thread2 causing the system to wait until it is done, then
+ * thread3 can start. 
+*/
+package threads_01_04;
+
+/**
+ *
+ * @author MFisher
+ */
+public class ThreadJoinExample {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        TestJoinClass t1 = new TestJoinClass("t1");  
+        TestJoinClass t2 = new TestJoinClass("t2");  
+        TestJoinClass t3 = new TestJoinClass("t3");  
+        t1.start();  
+        try{  
+            t1.join();  
+        }catch(Exception e){System.out.println(e);}  
+        t2.start(); 
+        //thread 3 won't start until thread 2 is complete
+        t3.start();  
+    }
+    
+}

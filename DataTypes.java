fa380fae44a39b59af38442853bc2ffd632920f2
Aman Tiwari <36691571+amantiwari8861@users.noreply.github.com>
2022-03-06 14:26:16 +0530

    slides added

--- DataTypes.java
+++ DataTypes.java
@@ -1,8 +1,9 @@
 class DataTypes
 {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         //8 data types
-        byte b=50;  //1 byte
+        byte b=127;  //1 byte
         short s=120; //2 bytes
         int a=10; //4 bytes
         long l=123456789; //8 bytes
@@ -11,5 +12,15 @@ class DataTypes
         char ch='A';   //in java char takes 2 bytes 
         boolean bool=true; //1 bit
         String name="AMan"; //in java String is a class 
+
+        System.out.println("byte b= "+b);
+        System.out.println("short s= "+s);
+        System.out.println("int a= "+a);
+        System.out.println("long l= "+l);
+        System.out.println("float pi= "+pi);
+        System.out.println("double d= "+d);
+        System.out.println("char ch= "+ch);
+        System.out.println("boolean bool= "+bool);
+        System.out.println("String name= "+name);
     }
 }

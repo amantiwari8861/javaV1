9a19a44774406560db65dda54720897289ad4377
Aman Tiwari <36691571+amantiwari8861@users.noreply.github.com>
2023-01-24 00:21:12 +0530

    Ip Address validator problem solved

---
+++ HackerRankSolutions/IpAddressValidator.java
@@ -0,0 +1,23 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
+class IpAddressValidator
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+
+    }
+}
+
+class MyRegex 
+{
+    String pattern="([01]?\\d{1,2}|2[0-4]\\d|25[0-5]).([01]?\\d{1,2}|2[0-4]\\d|25[0-5]).([01]?\\d{1,2}|2[0-4]\\d|25[0-5]).([01]?\\d{1,2}|2[0-4]\\d|25[0-5])";
+}
+
+// 0 to 255 => ([01]?\d{1,2}|2[0-4]\d|25[0-5])

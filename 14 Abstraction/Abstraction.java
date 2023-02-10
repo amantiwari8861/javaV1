759a3532bc8329c7d8a1b2a0c0ce5e0845169700
Aman Tiwari <36691571+amantiwari8861@users.noreply.github.com>
2023-02-10 14:09:22 +0530

    abstraction

--- 14 Abstraction/Abstraction.java
+++ 14 Abstraction/Abstraction.java
@@ -1,12 +1,12 @@
 abstract class Vehicle
 {
-     void model()
-     {
-         System.out.println("hello world ");
-     }
+    void model()
+    {
+       System.out.println("Default Model of BMW");
+    }
     abstract void speed();
     // {
-    //     System.out.println("i am a abstract fxn "); //error
+    //     System.out.println("i am a abstract fxn "); //error can't have body
     // }
 }
 class Bike extends Vehicle
@@ -15,10 +15,25 @@ class Bike extends Vehicle
     {
         System.out.println("max speed is 500km/h ");
     }
+    void showTires()
+    {
+        System.out.println("2 Tires");
+    }
+}
+public class Abstraction {
+
     public static void main(String[] args) {
+        
+        // Vehicle v=new Vehicle(); //error cannot be instantiated
+
+        // Bike b=new Bike();
+        // b.speed();
+        // b.model();
+        // b.showTires();
+        
         Vehicle obj=new Bike();
         obj.speed();
         obj.model();
-        // Vehicle v=new Vehicle(); //error
+        obj.showTires();
     }
 }

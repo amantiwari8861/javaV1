61e422e1ce513d99b128be13c324ddfef164bd14
Aman Tiwari <36691571+amantiwari8861@users.noreply.github.com>
2023-02-22 14:00:03 +0530

    java 8 interfaces

--- 15 Interfaces/InterfaceNewFeatures.java
+++ 15 Interfaces/InterfaceNewFeatures.java
@@ -1,32 +1,67 @@
-public interface ChartedAccountant {
+interface ChartedAccountant 
+{
     public abstract double deductFoodFee();
     public static final double HEALTH_INSURANCE_PERCENTAGE = 5.0;
-    public default double deductHealthInsurancePremium() {    }
+    public default double deductHealthInsurancePremium() { return 0.0;   }
+    public static void getHealthInsuranceCharge()
+    {
+        System.out.println("Current Health insurance charge is :"+HEALTH_INSURANCE_PERCENTAGE);
+    }
+    public default void askCadetails(String designation) 
+    {
+        if (designation.equalsIgnoreCase("Manager")) 
+        {
+            getCaDetails();
+        }
+    }
+    private void getCaDetails() 
+    {
+        System.out.println("Name :Aman");
+        System.out.println("Phone :981062743");
+        System.out.println("address :delhi");
+    }
 }
-
-public abstract class Employee implements ChartedAccountant {
+abstract class Employee implements ChartedAccountant 
+{
     public abstract double calculateSalary();
 }
-public class FullTimeEmployee extends Employee {
+class FullTimeEmployee extends Employee 
+{
     double employeeSalary = 50000;
-    public double calculateSalary() {
-    }
-    public double deductFoodFee() {
+    public double calculateSalary() { return employeeSalary;   }
+    public double deductFoodFee() 
+    { 
+        employeeSalary=employeeSalary-1000;  
+        return 1000;
     }
     public double deductHealthInsurancePremium() 
-    { // Default method getting overridden
+    { 
         return (HEALTH_INSURANCE_PERCENTAGE * employeeSalary) / 100;
     }
 }
-public class PartTimeEmployee extends Employee {
-    public double calculateSalary() {
-    }
+class PartTimeEmployee extends Employee 
+{
+    double empSal=20000;;
+    public double calculateSalary() {  return empSal;  }
 
-    public double deductFoodFee() {
+    public double deductFoodFee() 
+    {
+        empSal=empSal-1000;
+        return 1000;  
     }
 }
-public class InterfaceNewFeatures {
-    public static void main(String[] args) {
+public class InterfaceNewFeatures 
+{
+    public static void main(String[] args)
+    {
 
+        FullTimeEmployee fEmployee=new FullTimeEmployee();
+        
+        double charge=fEmployee.deductHealthInsurancePremium();
+        System.out.println(charge+" charge deducted from employee salary ");
+
+
+        ChartedAccountant.getHealthInsuranceCharge();
+        fEmployee.askCadetails("Manager");
     }
 }

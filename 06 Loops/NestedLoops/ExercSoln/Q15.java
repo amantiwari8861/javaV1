6313502220e63a820f19c3b70418bde44bc7a44a
Aman Tiwari <36691571+amantiwari8861@users.noreply.github.com>
2023-03-31 13:37:09 +0530

    nested loop Exercise question solved

--- 06 Loops/NestedLoops/ExercSoln/Q15.java
+++ 06 Loops/NestedLoops/ExercSoln/Q15.java
@@ -2,7 +2,21 @@ public class Q15 {
 
     public static void main(String[] args) {
         
-        
+        int spacepos=8,starpos=9;
+        for (int i = 1; i <= 16; i++) 
+        {
+            for (int j = 1; j <= 16; j++) 
+            {
+                if(spacepos>=j && i<=4 || spacepos>=8 && i<=8) 
+                {
+                    if(i==5)
+                        spacepos=6;
+                    System.out.print("@"); 
+                }
+            }
+            spacepos--;
+            System.out.println();
+        }
 
     }
 }

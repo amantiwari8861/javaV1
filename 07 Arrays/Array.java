28b21f7a31b7ea368aeccf0fa59e8ddd3f7d08fb
Aman Tiwari <36691571+amantiwari8861@users.noreply.github.com>
2023-04-04 13:05:31 +0530

    Arrays

--- 07 Arrays/Array.java
+++ 07 Arrays/Array.java
@@ -5,19 +5,23 @@ public class Array {
 
     public static void main(String[] args) {
        
-        // int arr[5]; //error
+        // int num;//1 block is created in memory
+        // int arr[5]; //error (not valid in java)
 
+        // new int[5];// creates 5 block of type integer
         int arr[]=new int[5];
         // int []arr1=new int[5];
         // int[] arr2=new int[5];
         // int arr[]={10,20,45,34,35,67,98};
+        // //or
         // int arr[]=new int[]{56,87,98,534,98,32};
 
-        // arr[0]=56;
-        // arr[1]=67;
-        // arr[2]=34;
-        // arr[3]=343;
-        // arr[4]=344;
+        // System.out.println("the value at array's 3rd index is "+arr[3]);
+        arr[0]=56;
+        arr[1]=67;
+        arr[2]=34;
+        arr[3]=343;
+        arr[4]=344;
         
         // System.out.println("Printing through normal for loop");
         // for(int i=0;i<5;i++)
@@ -32,30 +36,30 @@ public class Array {
         //     System.out.println(arr[i]);
         // }
 
-        // System.out.println("\nPrinting through for each loop");
+        // System.out.println("\nPrinting through for each loop (enhanced for loop)");
         // for(int e :arr)
         // {
         //     System.out.println(e);
         // }
 
-        // System.out.println("\nPrinting through Arrays.toString");
-        // System.out.println(Arrays.toString(arr));
+        System.out.println("\nPrinting through Arrays.toString");
+        System.out.println(Arrays.toString(arr));
 
-        Scanner sc=new Scanner(System.in);
+        // Scanner sc=new Scanner(System.in);
 
         //taking user defined value in fixed size array
 
-        System.out.println("enter arr :");
-        for(int i=0;i<arr.length;i++)
-        {
-            System.out.print("Enter "+i+"th element :");
-            arr[i]=sc.nextInt();
-        }
-        System.out.println("\nPrinting through array.length in for loop");
-        for(int i=0;i<arr.length;i++)
-        {
-            System.out.println(arr[i]);
-        }
+        // System.out.println("enter arr :");
+        // for(int i=0;i<arr.length;i++)
+        // {
+        //     System.out.print("Enter "+i+"th element :");
+        //     arr[i]=sc.nextInt();
+        // }
+        // System.out.println("\nPrinting through array.length in for loop");
+        // for(int i=0;i<arr.length;i++)
+        // {
+        //     System.out.println(arr[i]);
+        // }
 
 
         //taking user defined value in user defined sized array

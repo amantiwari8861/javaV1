eb9d734c6318e7ac0fcbb483f2a7ec0c3ad23b0a
Aman Tiwari <36691571+amantiwari8861@users.noreply.github.com>
2023-04-25 13:52:23 +0530

    constructor

--- 09 Class and Object/ConstructorDemo.java
+++ 09 Class and Object/ConstructorDemo.java
@@ -2,6 +2,10 @@ import java.util.Scanner;
 class Shape
 {
     int height,width;
+    Shape()
+    {
+        System.out.println("Default constructor");
+    }
     Shape(int h,int w)
     {
         height=h;
@@ -12,15 +16,17 @@ class Shape
         System.out.println("Area of the shape is :"+height*width);
     }
 }
-public class ConstructorDemo {
-public static void main(String[] args) {
-    
+public class ConstructorDemo 
+{
+public static void main(String[] args) 
+{
     int h,w;
     Scanner sc=new Scanner(System.in);
     System.out.println("Enter the height and width of the shape");
     h=sc.nextInt();
     w=sc.nextInt();
-    Shape obj=new Shape(h,w);
+    Shape obj=new Shape();
+    // Shape obj=new Shape(h,w);
     obj.area();
     sc.close();
     //Garbage Collection is a process in which the unused objects are collected and deleted from memory

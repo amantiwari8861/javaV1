6e5ed1eec386545e205712f8220a952078ef2003
Aman <amantiwari8861@gmail.com>
2023-08-03 11:52:46 +0530

    added immutable

---
+++ 16 Strings/Immutable.java
@@ -0,0 +1,22 @@
+class Immutable 
+{
+    public static void main(String[] args) 
+    {
+        String fname="Aman ";
+        String lname=" Tiwari";
+        // fname.concat(lname);
+        // System.out.println(fname);
+
+
+        String fullname=fname.concat(lname);
+        System.out.println("the fullname is "+fullname);
+        // or 
+        System.out.println("the full name is :"+fname.concat(lname));//concatination
+        System.out.println(fname);
+
+
+        fname=fname.concat(lname);//re-initialization
+        System.out.println("the value of str1 is :"+fname);//Aman Tiwari
+
+    }
+}

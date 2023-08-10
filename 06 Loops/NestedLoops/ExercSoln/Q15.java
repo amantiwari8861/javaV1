540035318c2db8f5f6204f9ba0525bb8535ea5be
Aman Tiwari <36691571+amantiwari8861@users.noreply.github.com>
2023-08-10 14:36:28 +0530

    updated exercise

--- 06 Loops/NestedLoops/ExercSoln/Q15.java
+++ 06 Loops/NestedLoops/ExercSoln/Q15.java
@@ -1,22 +1,69 @@
-public class Q15 {
+/*
+       *
+      * * 
+     * * *
+    * * * *
+     * * * 
+    * * * *
+   * * * * *
+  * * * * * *
+   * * * * * 
+  * * * * * *
+ * * * * * * *
+* * * * * * * *
+    * * * *
+    * * * *
+    * * * *
+    * * * *
+ */
 
-    public static void main(String[] args) {
-        
-        int spacepos=8,starpos=9;
-        for (int i = 1; i <= 16; i++) 
+public class Q15 
+{
+    public static void main(String[] args) 
+    {
+
+        int space=8-1,star=1,col=8;
+        for (int i = 1; i <=12; i++) 
         {
-            for (int j = 1; j <= 16; j++) 
+            for (int j = 1; j <= 8; j++) 
             {
-                if(spacepos>=j && i<=4 || spacepos>=8 && i<=8) 
+
+                if(j<=space)
                 {
-                    if(i==5)
-                        spacepos=6;
-                    System.out.print("@"); 
+                    System.out.print(" ");
+                }
+                else
+                {
+                    System.out.print("* ");
                 }
             }
-            spacepos--;
+            
+            if (i%4==0) 
+            {
+                space=space+1;// 7 -> 5 -> 3
+                star=star-1;//3->5
+            }
+            else
+            {
+                space=space-1;
+                star=star+1;
+            }
             System.out.println();
         }
-
+       for (int i = 0; i < 4; i++) 
+       {
+        for (int j = 0; j < 8; j++) 
+        {
+            if(j<4)
+            {
+                System.out.print(" ");
+            }
+            else
+            {
+                System.out.print("* ");
+            }
+        }
+        System.out.println();
+       }
     }
 }

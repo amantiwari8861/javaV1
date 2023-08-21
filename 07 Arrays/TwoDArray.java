01712f5c52e7ccb0b22387a8f58158d5849799e3
Aman Tiwari <36691571+amantiwari8861@users.noreply.github.com>
2023-08-21 14:29:43 +0530

    2d array

--- 07 Arrays/TwoDArray.java
+++ 07 Arrays/TwoDArray.java
@@ -5,31 +5,46 @@ public class TwoDArray {
     public static void main(String[] args) {
         
         String country[]={"India","America","Australia"};
-        int covidCases[][]=new int[3][2];//3 country and 2 state =>3*2 =6
+        String states[][]={
+                            {"Delhi","UP","Maharastra","Bihar"},
+                            {"New York","san deigo","texas","san fransisco"},
+                            {"victoria","south australia","tasmania","new south wales"}
+                        };
         // int covidCases[][]={
-        //     {10,34,67,4},
-        //     {223,67,57,67},
-        //     {23,567,517,7}
-        // };
+        //                         {10,34,67,4},
+        //                         {223,67,57,67},
+        //                         {23,567,517,7}
+        //                     };
+
+        int covidCases[][]=new int[3][4];//3 country and 4 state =>3*4 =12
 
         Scanner sc=new Scanner(System.in); 
 
-        System.out.println("Enter the data");
-
         for (int i = 0; i < 3; i++) 
         {
-            for (int j = 0; j < 2; j++) 
+            System.out.println("Enter the data of "+country[i]);
+            for (int j = 0; j < 4; j++) 
             {
+                System.out.print("\t"+states[i][j]+" :");
                 covidCases[i][j]=sc.nextInt();
             }
+            System.out.println();
         }
 
         System.out.println("The data are ");
-        for (int i = 0; i < covidCases.length; i++)  //3
+        // for (int i = 0; i < covidCases.length; i++)  //3
+        // {
+        //     System.out.println(country[i]+":");
+        //     for (int j = 0; j < covidCases[i].length; j++) //4
+        //     {
+        //         System.out.println("\t"+states[i][j]+":"+covidCases[i][j]);
+        //     }
+        // }
+        for (int i = 0; i < covidCases.length; i++) 
         {
-            for (int j = 0; j < covidCases[i].length; j++) //2
+            for (int j = 0; j < covidCases[i].length; j++) 
             {
-                System.out.print(covidCases[i][j]+"  ");
+                System.out.print(covidCases[i][j]+" ");
             }
             System.out.println();
         }

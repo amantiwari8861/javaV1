7f2b8157051efa811ca97adfe989427416906dab
Aman Tiwari <amantiwari8861@gmail.com>
2024-08-22 00:51:28 +0530

    reflection

---
+++ 20 Threads/ThreadLifeCycle.java
@@ -0,0 +1,22 @@
+public class ThreadLifeCycle extends Thread{
+    @Override
+    public void run() {
+        System.out.println("RUNNING"); // RUNNING
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        ThreadLifeCycle t1 = new ThreadLifeCycle();
+        System.out.println(t1.getState()); // NEW
+        t1.start();
+        System.out.println(t1.getState()); // RUNNABLE
+        Thread.sleep(100);
+        System.out.println(t1.getState()); // TIMED_WAITING
+        t1.join();
+        System.out.println(t1.getState()); // TERMINATED
+    }
+}
---
+++ 31 Reflection/TestReflection.java
@@ -0,0 +1,33 @@
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+public class TestReflection {
+
+    public static void main(String[] s) {
+
+        // Class c=String.class;
+
+        // System.out.println(c);
+        // c.getFields();
+        s=new String[5];
+        s[0]="java.lang.String";
+        try {
+            Class c = Class.forName(s[0]);
+            Method min[] = c.getDeclaredMethods();
+            System.out.println("****Methods are*****");
+            for (Method mm : min)
+                System.out.println(mm);
+            Field f[] = c.getDeclaredFields();
+            System.out.println("****Fields are*****");
+            for (Field ff : f)
+                System.out.println(ff);
+            Constructor con[] = c.getDeclaredConstructors();
+            System.out.println("****Constructors are*****");
+            for (Constructor cc : con)
+                System.out.println(cc);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
